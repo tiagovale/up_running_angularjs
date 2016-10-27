78eb84bba6229d22c3d74e7077b390fe353be7e2
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-10-27 10:56:30 -0200

    Filtering data and sorting them.

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,7 @@ myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
 
     $http.get('js/data.json').success(function(data) {
         $scope.artists = data;
+        $scope.artistOrder = 'name';
     });
 
 }]);
